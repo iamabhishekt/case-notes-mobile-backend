7fc0779f8d50a17598d90126f4f1dbd14e712b15
Abhishek Thakur <abhishekthakur.art@protonmail.com>
2022-12-05 03:53:51 -0500

    changes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
 
-  const config = new DocumentBuilder()
+  const options = new DocumentBuilder()
     .setTitle('Appointment API')
     .setDescription(
       'The API retrieves information about appointments with patient, doctor and schedule details.',
@@ -17,7 +17,7 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .build();
 
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT || 3000);
